da5873a6803d6ff1c0d1796e3db742e48d9e5d28
zinoki <zinouye@gmail.com>
2018-01-23 11:22:33 -0800

    added background

--- src/rickDancer.js
+++ src/rickDancer.js
@@ -2,10 +2,9 @@ var rickDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this);
   this.$node = $('<div class="dancer"><img src="src/rick.gif" width="158.5" height="115.5"></div>');
   this.setPosition(top, left);
-  this.setPosition(this.top, this.left);
+  // this.setPosition(this.top, this.left);
 };
 
 rickDancer.prototype = Object.create(makeDancer.prototype);
-rickDancer.prototype.roll = function(){};
 rickDancer.prototype.constructor = rickDancer;
 
