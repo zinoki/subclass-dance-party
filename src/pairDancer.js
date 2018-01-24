d0665d528f203060ab40a62329b2acdf79ab50ed
zinoki <zinouye@gmail.com>
2018-01-23 19:05:07 -0800

    added interaction feature of pairDancer

--- src/pairDancer.js
+++ src/pairDancer.js
@@ -3,8 +3,12 @@ var pairDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<div class="dancer"><img src="src/linkDancer.gif" width="158.5" height="115.5"></div>');
   this.setPosition(top, left);
   this.findNearest = function() {
-    
+    var friend = Math.floor(Math.random() * (window.dancers.length - 1));
+    var friendTop = window.dancers[friend].$node[0].style.top;
+    var friendLeft = window.dancers[friend].$node[0].style.left;
+    this.$node.animate({top: friendTop, left: friendLeft});
   };
+  this.findNearest();
   // this.setPosition(this.top, this.left);
 };
 
